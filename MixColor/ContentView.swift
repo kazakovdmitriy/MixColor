da208f7fa69afebcc441c53c48d81587c7f4dfd4
Дмитрий Казаков <dmitryifile@yandex.ru>
2025-01-20 22:31:51 +0300

    Добавил навигацию

--- MixColor/ContentView.swift
+++ MixColor/ContentView.swift
@@ -17,34 +17,37 @@ struct ContentView: View {
     }
     
     var body: some View {
-        
-        VStack {
-            ColorRectangle(
-                size: 80,
-                text: color1.getColorName(),
-                color: color1
-            )
-            .padding()
-            
-            Text("+")
-                .font(.system(size: 30))
-            
-            ColorRectangle(
-                size: 80,
-                text: color2.getColorName(),
-                color: color2
-            )
-            .padding()
-            
-            Text("=")
-                .font(.system(size: 30))
-            
-            ColorRectangle(
-                size: 80,
-                text: mixColor.getColorName(),
-                color: mixColor
-            )
-            .padding()
+        NavigationStack {
+            VStack {
+                ColorRectangle(
+                    size: 80,
+                    text: color1.getColorName(),
+                    color: color1
+                )
+                .padding()
+                
+                Text("+")
+                    .font(.system(size: 30))
+                
+                ColorRectangle(
+                    size: 80,
+                    text: color2.getColorName(),
+                    color: color2
+                )
+                .padding()
+                
+                Text("=")
+                    .font(.system(size: 30))
+                
+                ColorRectangle(
+                    size: 80,
+                    text: mixColor.getColorName(),
+                    color: mixColor
+                )
+                .padding()
+            }
+            .navigationTitle("Color Mixer")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
