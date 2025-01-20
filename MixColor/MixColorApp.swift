fd733f74fadb231d05480fa65fe12dea9a316b70
Дмитрий Казаков <dmitryifile@yandex.ru>
2025-01-20 21:17:23 +0300

    Initial Commit

---
+++ MixColor/MixColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MixColorApp.swift
+//  MixColor
+//
+//  Created by Дмитрий on 20.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct MixColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
